0bf5cff2e1365f28283becfbd6788e8657acaf57
Wes <gkm85663@gmail.com>
2020-06-06 09:55:48 +0800

    Delete suo file from repository

---
+++ Christmas/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ChristmasTests;
+
+namespace Christmas
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            new Holiday()
+        }
+    }
+}
--- ChristmasTests/ChristmasTests.cs
+++ ChristmasTests/ChristmasTests.cs
@@ -4,18 +4,19 @@ namespace ChristmasTests
 {
     public class ChristmasTests
     {
-        public class Holiday
+    }
+
+    public class Holiday
+    {
+        public void SayHello()
         {
-            public void SayHello()
+            if (DateTime.Today.Month == 12 && DateTime.Today.Day == 25)
             {
-                if (DateTime.Today.Month == 12 && DateTime.Today.Day == 25)
-                {
-                    Console.WriteLine("Today is Christmas");
-                }
-                else
-                {
-                    Console.WriteLine("Today is not Christmas");
-                }
+                Console.WriteLine("Merry Christmas");
+            }
+            else
+            {
+                Console.WriteLine("Today is not Christmas");
             }
         }
     }
