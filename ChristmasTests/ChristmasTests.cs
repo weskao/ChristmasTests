8f862625c0eea59189f4413b89b6480070c87ee6
Wes <gkm85663@gmail.com>
2020-06-06 10:46:37 +0800

    remove cache

--- ChristmasTests/ChristmasTests.cs
+++ ChristmasTests/ChristmasTests.cs
@@ -1,26 +1,52 @@
 using System;
-using NSubstitute;
 using NUnit.Framework;
 
 namespace ChristmasTests
 {
+    public class ChristmasTests
+    {
+        [Test]
+        public void Today_Is_xmas()
+        {
+            var holidayForTest = new HolidayForTest();
+            holidayForTest.SetToday(12, 25);
+
+            Assert.AreEqual("Merry X'mas", holidayForTest.SayHello());
+        }
+    }
+
     public class Holiday
     {
         public string SayHello()
         {
-            if (GetToday().Month == 12 && DateTime.Today.Day == 25)
+            if (GetToday().Month == 12 && GetToday().Day == 25)
             {
-                return "Merry Christmas";
+                return "Merry X'mas";
             }
             else
             {
-                return "Today is not Christmas";
+                return "Today is not X'mas";
             }
         }
 
-        protected DateTime GetToday()
+        protected virtual DateTime GetToday()
         {
             return DateTime.Today;
+        }
+    }
+
+    public class HolidayForTest : Holiday
+    {
+        private DateTime _today;
+
+        protected override DateTime GetToday()
+        {
+            return _today;
+        }
+
+        public void SetToday(int month, int day)
+        {
+            _today = new DateTime(2025, month, day);
         }
     }
 }
